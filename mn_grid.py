9b6f3c8f211206cdb25c0ee4a085c8032fd5037c
John Asplund <30665264+cecilartavion@users.noreply.github.com>
2019-04-01 12:33:21 -0400

    Add files via upload

--- mn_grid.py
+++ mn_grid.py
@@ -7,8 +7,8 @@ Created on Sat Dec  1 17:40:16 2018
 
 import numpy as np
 
-n=29
-m=39
+n=7
+m=10
 d = n+m
 
 if ((n+m) % 2) == 1:
@@ -23,18 +23,21 @@ else:
 print('m={}, n={}, k={}'.format(m,n,k))
 mat = np.zeros((n+1,m+1))
 
+#for cross
 for x in range(0,int(n/2)):
     mat[int(n/2),int(n/2)-x-1]= 2*(x+1)
     mat[int(n/2)-1-x,int(n/2)]= 2*(x+1)
     mat[int(n/2),int(n/2)+2+x-1]= 2*(x+1)
     mat[int(n/2)+1+x,int(n/2)]= 2*(x+1)
-    
+
+#for those that appear 3 times
 for x in range(0,int((k-n)/2)+1):
     a = 2*int((n-1)/2)+2+2*x
     mat[0,int(a/2)] = a
     mat[int(a/2),0] = a
     mat[n,int(a/2)+(a-n)]=a
-    
+
+#largest numbers 
 for x in range(int((n+m-k)/2)):
     mat[0+x,m] = d-2*x
     mat[n,0+x] = d-2*x  
@@ -53,11 +56,12 @@ if int(n/2)-1> int((n+m-k)/2):
 mat[n*eps,int(n/2)-1] = d-2*(int(n/2)-1)-1+eps
 mat[(int(n/2)-1)+2-2*eps,m-1+eps] = d-2*(int(n/2)-1)-1+eps
 
-for x in range(0,int(n/2)-1):
+
+for x in range(0,int(n/2)):
     mat[n*eps+x*(-1)**eps+1*(-1)**eps,int(n/2)-1] = d-2*(int(n/2)-1)-1+eps-2-2*x
     mat[(int(n/2)-1)+2-2*eps,m-2+eps-x] = d-2*(int(n/2)-1)-1+eps-2-2*x 
 
-small_nums = (d-2*(int(n/2)-1)-1+eps-2-2*(int(n/2)-2))
+small_nums = (d-2*(int(n/2)-1)-1+eps-2-2*(int(n/2)-1))
 for x in range(1,small_nums,2):
     mat[1,int(n/2)+1+int(x/2)] = small_nums-x-1
     mat[1,int(n/2)+1+int(small_nums/2)+int((x-1)/2)] = x
